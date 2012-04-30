c18b1c93d354631dbb57a8ff727154a245036352
Tute Costa <tutecosta@gmail.com>
2012-04-30 13:21:04 -0400

    Some adjustments.

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 3. Run <tt>rake db:migrate</tt>
 4. Run <tt>rails g parrot MODEL_NAME</tt>
 5. Add subresources to routes
+
      resources RESOURCE_NAME do
        resources :parrot_comments, controller: 'parrot/comments', path: 'comments'
      end
+
+# Notes
+
+* By default it calls to_s on commenter name. You may want to alias it to name
+  for example, so it doesn't show the Ruby "ugly" object.
--- app/controllers/parrot/comments_controller.rb
+++ app/controllers/parrot/comments_controller.rb
@@ -3,7 +3,7 @@ module Parrot
     respond_to :html
 
     def index
-      @comments = Comment.where(commentable_type: commentable_type.capitalize, commentable_id: commentable_id)
+      @comments = Comment.where(commentable_type: commentable_type.classify, commentable_id: commentable_id)
       respond_with @comments
     end
 
@@ -16,12 +16,33 @@ module Parrot
 
     def create
       @comment = Comment.new(params[:parrot_comment])
-      @comment.commentable_type = commentable_type.capitalize
+      @comment.commentable_type = commentable_type.classify
       @comment.commentable_id = commentable_id
       @comment.author_id = current_user.id
       @comment.save
       @commentable_type = commentable_type
       respond_with @comment, :location => commentable
+    end
+
+    # Following methods should belong to ApplicationController
+    def commentable_fk
+      commentable_fk = params.select{|k,v| k =~ /_id/ }.keys.first
+    end
+
+    def commentable_type
+      commentable_fk.gsub('_id', '')
+    end
+
+    def commentable_id
+      params[commentable_fk]
+    end
+
+    def commentable
+      commentable_type.classify.constantize.find(commentable_id)
+    end
+
+    def debug(object)
+      logger.warn "DEBUGGING: #{object.inspect}"
     end
   end
 end
